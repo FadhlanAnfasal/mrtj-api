b156c93ccb34a3845d264aaa884ade55d8a63532
FadhlanAnfasal <fadhlananfasalhajji1403@mail.ugm.ac.id>
2025-10-27 12:26:11 +0700

    first commit - mrtj api

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const PORT = 3000;
+const dataPath = "./hasil_scrapper_mrtj.json";
+
+// Baca data JSON
+const data = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+
+app.get("/", (req, res) => {
+  res.json(data); // langsung kirim semua isi file JSON
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log("Server aktif di port: " + PORT);
+});
+
